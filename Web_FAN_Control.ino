de81e7f953fc39e5b701bf757e33ee59e650a836
Nikolay <nikktro@gmail.com>
2018-07-03 00:06:38 +0300

    OTA function

--- Web_FAN_Control.ino
+++ Web_FAN_Control.ino
@@ -114,7 +114,12 @@ void setup(void){
 
   });
   server.on("/state", [](){
-    server.send(200, "text/html", checkState());
+    server.send(200, "text/html", checkstate());
+    delay(100);
+
+  });
+  server.on("/flash", [](){
+    server.send(200, "text/html", flash());
     delay(100);
 
   });
@@ -123,6 +128,36 @@ void setup(void){
     delay(100);
 
   });
+
+   server.on("/update", HTTP_POST, [](){
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
+      ESP.restart();
+    },[](){
+      HTTPUpload& upload = server.upload();
+      if(upload.status == UPLOAD_FILE_START){
+        Serial.setDebugOutput(true);
+        WiFiUDP::stopAll();
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+        if(!Update.begin(maxSketchSpace)){//start with max available size
+          Update.printError(Serial);
+        }
+      } else if(upload.status == UPLOAD_FILE_WRITE){
+        if(Update.write(upload.buf, upload.currentSize) != upload.currentSize){
+          Update.printError(Serial);
+        }
+      } else if(upload.status == UPLOAD_FILE_END){
+        if(Update.end(true)){ //true to set the size to the current progress
+          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+          Update.printError(Serial);
+        }
+        Serial.setDebugOutput(false);
+      }
+      yield();
+    });
+
   server.begin();
   Serial.println("HTTP server started");
 
@@ -271,10 +306,16 @@ String resume()
   return(web);
 }
 
-String checkState()
+String checkstate()
 {
   String web;
   web += state;
+  return(web);
+}
+
+String flash()
+{
+  const char* web = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
   return(web);
 }
 
